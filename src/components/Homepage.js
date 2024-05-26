25746cd3190981716a4aabc5f593633380725038
Ahlam <Ahlam.musemohamed@gmail.com>
2024-05-26 15:00:55 -0500

    did testimonials responsive

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Hero from './Hero';
 import Specials from './Specials';
-// import Testimonial from './Testimonial';
+import Testimonial from './Testimonial';
 
 function Homepage(){
     return(
         <>
             <Hero/>
             <Specials/>
-            {/* <Testimonial/> */}
+            <Testimonial/>
         </>
     )
 }
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -4,58 +4,47 @@ function Testimonial(){
     return(
         <div className="testimonial">
 
-           <h2>Testimonial</h2>
+           <div><h2>Testimonial</h2> </div>
 
            <div className="ratingsContainer">
-                <div className="box">
-                    <img src="./images/star.jpg" alt="star rating" className="ratings"/>
-                    <div className="rater">
-                        <img src="./images/profile.webp" alt="profile picture"/>
+                <div className="rating">
+
+                    <img src="./images/star.jpg" alt="star rating" className="starImg"/>
+                    <div className="personRating">
+                        <img src="./images/profile.webp" alt="profile picture" className="profilePic"/>
                         <p>John Smith</p>
                     </div>
-                    <div className="personRating">
                     <p>Ad dolore nisi anim deserunt. Do eiusmod culpa minim eu ullamco sunt excepteur reprehenderit est cupidatat consectetur. </p>
-
-                    </div>
                 </div>
-                {/* n */}
-                <div className="box">
-                    <img src="./images/star.jpg" alt="star rating" className="ratings"/>
-                    <div className="rater">
-                        <img src="./images/profile.webp" alt="profile picture"/>
+                <div className="rating">
+
+                    <img src="./images/star.jpg" alt="star rating" className="starImg"/>
+                    <div className="personRating">
+                        <img src="./images/profile.webp" alt="profile picture" className="profilePic"/>
                         <p>John Smith</p>
                     </div>
-                    <div className="personRating">
                     <p>Ad dolore nisi anim deserunt. Do eiusmod culpa minim eu ullamco sunt excepteur reprehenderit est cupidatat consectetur. </p>
-
-                    </div>
                 </div>
-                {/* n */}
-                <div className="box">
-                    <img src="./images/star.jpg" alt="star rating" className="ratings"/>
-                    <div className="rater">
-                        <img src="./images/profile.webp" alt="profile picture"/>
+                <div className="rating">
+
+                    <img src="./images/star.jpg" alt="star rating" className="starImg"/>
+                    <div className="personRating">
+                        <img src="./images/profile.webp" alt="profile picture" className="profilePic"/>
                         <p>John Smith</p>
                     </div>
-                    <div className="personRating">
                     <p>Ad dolore nisi anim deserunt. Do eiusmod culpa minim eu ullamco sunt excepteur reprehenderit est cupidatat consectetur. </p>
-
-                    </div>
                 </div>
+                <div className="rating">
 
-                {/* n */}
-                <div className="box">
-                    <img src="./images/star.jpg" alt="star rating" className="ratings"/>
-                    <div className="rater">
-                        <img src="./images/profile.webp" alt="profile picture"/>
+                    <img src="./images/star.jpg" alt="star rating" className="starImg"/>
+                    <div className="personRating">
+                        <img src="./images/profile.webp" alt="profile picture" className="profilePic"/>
                         <p>John Smith</p>
                     </div>
-                    <div className="personRating">
                     <p>Ad dolore nisi anim deserunt. Do eiusmod culpa minim eu ullamco sunt excepteur reprehenderit est cupidatat consectetur. </p>
-
-                    </div>
                 </div>
             </div>
+            
 
         </div>
     )
